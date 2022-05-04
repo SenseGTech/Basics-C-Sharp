2b28ae43d25bbf520575d9e114cd513e21f04ca4
George Stankov <91672999+SenseGTech@users.noreply.github.com>
2022-05-04 16:40:09 +0300

    Add files via upload
    Just testing variables

---
+++ MyFirstprogram/MyFirstprogram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyFirstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x; //declaration
+            x = 123; //initalizaition
+
+            int y = 321; //declaration + initialization
+            int z = x + y;
+            int age = 21;
+            double height = 180.2;
+            bool alive = true;
+            char symbol = '@';
+            String name = "George";
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Your age is " +  age);
+            Console.WriteLine("Your height is " +  height);
+            Console.WriteLine("Are you alive " +  alive);
+            Console.WriteLine("Your symbol is " +  symbol);
+            String userName = symbol + name;
+            Console.WriteLine("Your username is: " + userName);
+           
+            Console.ReadKey();
+
+        }
+    }
+}
